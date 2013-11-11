4ee055daa1c68becc8dfbd25dc4f9008664c3998
octomarat <maratx387@gmail.com>
2013-11-11 19:06:27 +0400

    Sliding screens eventually created!

--- src/AU/MightyFour/Sitcomizer/MainActivity.java
+++ src/AU/MightyFour/Sitcomizer/MainActivity.java
@@ -2,16 +2,40 @@ package AU.MightyFour.Sitcomizer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity
 {
-	/**
-	 * Called when the activity is first created.
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LayoutInflater inflater = LayoutInflater.from (this);
+        List<View> pages = new ArrayList<View>();
+
+        View page = inflater.inflate (R.layout.buttons_view, null);
+        pages.add (page);
+
+        page = inflater.inflate (R.layout.buttons_view, null);
+        Button laugh_button = (Button) page.findViewById(R.id.central_button);
+        laugh_button.setText("Laugh!");
+        pages.add (page);
+
+        page = inflater.inflate(R.layout.oneliners_view, null);
+        pages.add (page);
+
+        CustomPagerAdapter pagerAdapter = new CustomPagerAdapter(pages);
+        ViewPager viewPager = new ViewPager(this);
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.setCurrentItem(1);
+
+        setContentView(viewPager);
+    }
 }
